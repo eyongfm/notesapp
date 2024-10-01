ab02980a405c9e7a8e58df9f2e3c99d4d26508bb
eyongfm <fmeyong01@yahoo.fr>
2024-10-01 10:48:49 +0200

    test second build

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + Amplify on AWS</h1>
+      <h1>Test second CICD </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
